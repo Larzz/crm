ab4e70b755a98ea7587f970857aa6933e948aa7b
Larry Parba <larz.parba@gmail.com>
2022-02-09 14:01:39 +0400

    update date

--- resources/js/components/staff/LeaveApplicationComponent.vue
+++ resources/js/components/staff/LeaveApplicationComponent.vue
@@ -12,17 +12,49 @@
                 <div class="row">
                     <div class="col-xl-8" id="date-picker">
                         <div class="row">
-                            <div class="col-md-6">
-                                <h4>From</h4>
-                                <input v-model="field.leave_from" type="date" class="form-control">
-                                <h5 v-html="field.leave_from"></h5>
+                            <div class="col-md-12">
+                                <h4>Date</h4>
+                                <!-- <input v-model="field.leave_from" type="date" class="form-control"> -->
+                                <!-- <h5 v-html="field.leave_from"></h5> -->
+                                <!-- <v-date-picker v-model="field.date" is-range /> -->
+                                <v-date-picker 
+                                    :available-dates='{ start: new Date(), end: null }'
+                                    v-model="field.date" is-range>
+                                    <template v-slot="{ inputValue, inputEvents }">
+                                        <div class="flex justify-center items-center">
+                                        <input
+                                            :value="inputValue.start"
+                                            v-on="inputEvents.start"
+                                            class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300"
+                                        />
+                                        <svg
+                                            width="25px"
+                                            class="w-4 h-4 mx-2"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                        >
+                                            <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M14 5l7 7m0 0l-7 7m7-7H3"
+                                            />
+                                        </svg>
+                                        <input
+                                            :value="inputValue.end"
+                                            v-on="inputEvents.end"
+                                            class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300"
+                                        />
+                                        </div>
+                                    </template>
+                                </v-date-picker>
                             </div>
-
-                            <div class="col-md-6">
+                            <!-- <div class="col-md-6">
                                 <h4>To</h4>
                                 <input v-model="field.leave_to" @change="validate" type="date" class="form-control">
                                 <h5 v-html="field.leave_to"></h5>
-                            </div>
+                            </div> -->
                         </div>
                     </div>
 
@@ -64,7 +96,11 @@
 </template>
 
 <script>
+    import VCalendar from 'v-calendar';
     export default {
+        components: {
+            VCalendar: 'v-calendar'
+        },
         data() {
             return {
                 field: {
@@ -73,8 +109,14 @@
                     total_day_used: 0,
                     remaining_days: 0,
                     used_days: 0,
-                    leave_id: null
+                    leave_id: null,
+                    date: null,
                 },
+                range: {
+                    start: new Date(2020, 0, 1),
+                    end: new Date(2020, 0, 5),
+
+                }
             }
         },
         props: ['leave'],
@@ -111,12 +153,50 @@
         },
         methods: {
 
+            // Expects start date to be before end date
+            // start and end are Date objects
+            dateDifference(start, end) {
+
+            // Copy date objects so don't modify originals
+            var s = new Date(+start);
+            var e = new Date(+end);
+            
+            // Set time to midday to avoid dalight saving and browser quirks
+            s.setHours(12,0,0,0);
+            e.setHours(12,0,0,0);
+            
+            // Get the difference in whole days
+            var totalDays = Math.round((e - s) / 8.64e7);
+            
+            // Get the difference in whole weeks
+            var wholeWeeks = totalDays / 7 | 0;
+            
+            // Estimate business days as number of whole weeks * 5
+            var days = wholeWeeks * 5;
+
+            // If not even number of weeks, calc remaining weekend days
+            if (totalDays % 7) {
+                s.setDate(s.getDate() + wholeWeeks * 7);
+                
+                while (s < e) {
+                s.setDate(s.getDate() + 1);
+
+                // If day isn't a Sunday or Saturday, add to business days
+                if (s.getDay() != 0 && s.getDay() != 6) {
+                    ++days;
+                }
+                }
+            }
+            return days + 1;
+            },
+
             validate() {
-
-
 
                 this.field.remaining_days = this.leave.available_days
                 this.field.used_days = this.leave.used_days
+
+                this.field.leave_from = this.field.date.start
+                this.field.leave_to = this.field.date.end
 
                 const date1 = new Date(this.field.leave_from);
                 const date2 = new Date(this.field.leave_to);
@@ -136,6 +216,12 @@
 
             submitVacation() {
 
+                // this.validate()
+
+                console.log(this.dateDifference(this.field.date.start, this.field.date.end))
+                console.log(this.field.date)
+                
+                return;
                 let $this = this
 
                 if (!this.field.leave_from) {
@@ -147,6 +233,9 @@
                     $this.$toastr.e('Leave to is Required')
                     return;
                 }
+
+                console.log(this.field)
+                return;
 
                 JsLoadingOverlay.show(this.$configs);
 
--- resources/views/staff/pages/index.blade.php
+++ resources/views/staff/pages/index.blade.php
@@ -1,63 +1,70 @@
 @extends('staff.layouts.default')
 
 @section('content')
- 
-  <!-- Main content -->
-  <div class="main-content" id="panel">
+
+<!-- Main content -->
+<div class="main-content" id="panel">
     <!-- Topnav -->
     @include('staff.includes.top_nav')
     <!-- Header -->
     <div class="header bg-primary pb-6">
-      <div class="container-fluid">
-          <div class="header-body">
-              <div class="row align-items-center py-4">
-                  <div class="col-lg-12 col-7">
-                      <ul class="nav-sub-sx">
-                          <li> <a href="">Blog</a> </li>
-                          <li> <a href="">Presentations</a> </li>
-                          <li> <a href="">Client Backend</a> </li>
-                          <li> <a href="">Website</a> </li>
-                          <li> <a href="">Newsletter</a> </li>
-                          <li> <a href="">Tutorials</a> </li>
-                      </ul>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
-    
+        <div class="container-fluid">
+            <div class="header-body">
+                <div class="row align-items-center py-4">
+                    <div class="col-lg-12 col-7">
+                        <ul class="nav-sub-sx">
+                            <li> <a class="{{ $active === 'dashboard' ? 'active' : '' }}"
+                                    href="{{ route('staff.home') }}">Dashboard</a> </li>
+                            <li> <a class="{{ $active === 'backend' ? 'active' : '' }}"
+                                    href="{{ route('admin.client') }}">Client</a> </li>
+                            <li> <a class="{{ $active === 'website_domains' ? 'active' : '' }}"
+                                    href="{{ route('website.index') }}">Website</a> </li>
+                            <li> <a class="{{ $active === 'newsletter' ? 'active' : '' }}"
+                                    href="{{ route('newsletter.index') }}">Newsletter</a> </li>
+                            <li> <a class="{{ $active === 'tutorials' ? 'active' : '' }}"
+                                    href="{{ route('tutorial.index') }}">Tutorials</a> </li>
+                            <li> <a href="https://creativouae.com/articles-and-newsletters/">Blog</a> </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- Page content -->
     <div class="container-fluid mt--6">
-      <div class="row">
-        <div class="col-xl-6">
-          <general-bulletin-card> </general-bulletin-card>
+        <div class="row">
+            <div class="col-xl-6">
+                <general-bulletin-card> </general-bulletin-card>
+            </div>
+            <div class="col-xl-6">
+                <staff-leave-application :leave="{{ $leave }}"> </staff-leave-application>
+            </div>
         </div>
-        <div class="col-xl-6">
-          <staff-leave-application :leave="{{ $leave }}"> </staff-leave-application>
-        </div>
-      </div>
-      <div class="row">
+        <div class="row">
 
-        <div class="col-xl-4">
-          <staff-profile :employee="{{ Auth::User() }}"></staff-profile>
-        </div>
+            <div class="col-xl-4">
+                <staff-profile :employee="{{ Auth::User() }}"></staff-profile>
+            </div>
 
-        <div class="col-xl-4">
-          <staff-document></staff-document>
-        </div>
+            <div class="col-xl-4">
+                <staff-document></staff-document>
+            </div>
 
-        <div class="col-xl-4">
-          <general-client-card></general-client-card>
+            <div class="col-xl-4">
+                <general-client-card></general-client-card>
+            </div>
+
         </div>
 
-      </div>
+        <div class="col-lx-12">
+            <linked-in></linked-in>
+        </div>
 
-      <div class="col-lx-12">
-          <linked-in></linked-in>
-      </div>    
-
-      <!-- Footer -->
-      @include('staff.includes.footer')
+        <!-- Footer -->
+        @include('staff.includes.footer')
     </div>
-  </div>
+    
+</div>
 @endsection
