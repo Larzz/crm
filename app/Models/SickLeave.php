d794db3c931c21cdf92e3f0f35e33326665bf089
Larry Parba <larz.parba@gmail.com>
2023-11-22 11:47:59 +0400

    uu

--- app/Models/SickLeave.php
+++ app/Models/SickLeave.php
@@ -2,11 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
 class SickLeave extends Model
 {
-    use HasFactory;
     protected $table = 'sick_leave';
 }
